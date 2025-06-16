797c159367d673cacfc20e69def0d59ff38d65cc
klynton Faustino <klynton19@gmail.com>
2025-06-16 11:45:14 -0300

    Refatora cálculos no routes.py e adiciona lógica em services.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,21 @@
-from fastapi import APIRouter
+#app/routes.py
+
+from fastapi import APIRouter, Query, HTTPException
+from app.services import calcular_comissao
 
 router = APIRouter()
 
 @router.get("/comissao")
-def calcular_comissao(valor_carga: float, percentual_comissao: float):
-    comissao = (valor_carga * percentual_comissao) / 100
-    return {
-        "valor_carga": valor_carga,
-        "percentual_comissao": percentual_comissao,
-        "comissao": comissao
+def calcular_comissao(
+    valor_carga: float = Query(..., gt=0, description="valor total da carga"),
+    percentual_comissao: float = Query(..., gt=0, description="percentual da comissão")
+):
+    try:
+        valor_comissao = calcular_comissao(valor_carga, percentual_comissao)
+        return {
+            "valor_carga": valor_carga,
+            "percentual_comissao": percentual_comissao,
+            "valor_comissao": round(valor_comissao,2)
         }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Erro no cálculo: {str(e)}")
